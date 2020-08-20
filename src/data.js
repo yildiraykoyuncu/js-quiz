980e0a2d73b515124231b663c5e90f1cfd111e7d
Yıldıray Koyuncu <61549573+yildiraykoyuncu@users.noreply.github.com>
2020-08-20 16:51:52 +0200

    added question display close #3

--- src/data.js
+++ src/data.js
@@ -6,39 +6,42 @@
 */
 
 const state = {
-  // how many questions has the user answered?
-  //  you can calculate this value by iterating through the questions
-  //  is selected === -1?
-  answered: 0,
-  // how many correct answers has the user submitted?
-  //  you can calculate this value by iterating through the questions
-  //  is selected === correct?
-  correct: 0,
-  // should the correct answer be indicated when a user is wrong?
-  //  a possible user setting, this could be toggled in the UI
-  showCorrectAnswers: false,
-  // the questions in the quiz
-  questions: [
-    {
-      text: 'What are the different ways to declare a JS variable?',
-      answers: [
-        'constant, let, variable',
-        'var, const, let, function',
-        'var, let, const'
-      ],
-      correct: 2,
-      selected: -1,
+    game: {
+        //index of question currently displayed
+        currentQuestion: 1,
+        // how many questions has the user answered?
+        //  you can calculate this value by iterating through the questions
+        //  is selected === -1?
+        answered: 0,
+        // how many correct answers has the user submitted?
+        //  you can calculate this value by iterating through the questions
+        //  is selected === correct?
+        correct: 0,
+        // should the correct answer be indicated when a user is wrong?
+        //  a possible user setting, this could be toggled in the UI
+        showCorrectAnswers: false,
+        // the questions in the quiz
     },
-    {
-      text: 'What does `typeof` do?',
-      answers: [
-        'changes the type of a primitive value',
-        'returns a string describing the type of a value',
-        'determines if a value is primitive',
-        'can tell the difference between arrays and objects',
-      ],
-      correct: 1,
-      selected: -1,
-    }
-  ]
+    questions: [{
+            text: 'What are the different ways to declare a JS variable?',
+            answers: [
+                'constant, let, variable',
+                'var, const, let, function',
+                'var, let, const'
+            ],
+            correct: 2,
+            selected: -1,
+        },
+        {
+            text: 'What does `typeof` do?',
+            answers: [
+                'changes the type of a primitive value',
+                'returns a string describing the type of a value',
+                'determines if a value is primitive',
+                'can tell the difference between arrays and objects',
+            ],
+            correct: 1,
+            selected: -1,
+        }
+    ]
 };
--- src/handlers/quizstarthandler.js
+++ src/handlers/quizstarthandler.js
@@ -4,6 +4,37 @@ console.log('--- loading handler: _');
 
 const quizStartHandler = (event) => {
     console.log('hello')
+
+    // read from data
+    const questionIndex = state.game.currentQuestion;
+    const question = state.questions[questionIndex];
+
+    //Create HTML
+
+    const html = `<div class="content-container">
+    <p>${question.text}<p>
+
+    <input type="radio" name="answer" value="0" id="answer_0">
+    <label for="answer_0">${question.answers[0]}</label><br>
+
+    <input type="radio" name="answer" value="0" id="answer_1">
+    <label for="answer_0">${question.answers[1]}</label><br>
+
+    <input type="radio" name="answer" value="0" id="answer_2">
+    <label for="answer_0">${question.answers[2]}</label>
+    </div>
+
+    <div class="interaction">
+    
+    <button id="confirm" class="btn">Confirm!</button>
+
+    </div>
+    
+    `
+        //Render HTML to DOM
+
+    document.getElementById('interface').innerHTML = html
+    document.getElementById('interface').classList.add('container')
 };
 
 /* handlers define user interactions
