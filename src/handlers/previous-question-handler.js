9822f82bb9fa77bbd99b317afd7b707fe7a9f991
eltayeb <gaaliii@yahoo.com>
2020-08-23 00:11:29 +0200

    previous-question-handler-script

---
+++ src/handlers/previous-question-handler.js
@@ -0,0 +1,33 @@
+'use strict';
+
+console.log('--- loading handler: previous-question-handler');
+
+const previousQuestionHandler = (event) => {
+    debugger;
+
+    if (event.target.id !== 'previous-question') return;
+    
+    // update current question index
+    state.game.currentQuestion--;
+
+    //read from state
+    const questionIndex = state.game.currentQuestion;
+    const question = state.questions[questionIndex];
+
+    //Create HTML
+
+    const html = questionDisplay(question)
+        //Render HTML to DOM
+
+    document.getElementById('interface').innerHTML = html
+
+    // check if current question is 0 then not to display prev button or last question so not to show next button
+    
+    const next = document.getElementById('next-question');
+    const previous = document.getElementById('previous-question');
+    const questionsArr = state.questions;
+
+    next.style.display = checkLast(questionIndex, questionsArr);
+    previous.style.display = checkFirst(questionIndex, questionsArr);
+    
+}
